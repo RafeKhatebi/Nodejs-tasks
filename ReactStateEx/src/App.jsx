fceab4490477e491db103923982f5b26d1c3ab0c
Rafe Khatebi <rafekhatebi@gmail.com>
2026-05-22 09:57:20 +0430

    updated the app

--- ReactStateEx/src/App.jsx
+++ ReactStateEx/src/App.jsx
@@ -3,30 +3,6 @@ import { useState } from "react"
 export default function App() {
   return (
     <div>
-      <Itemlist />
-    </div>
-  )
-}
-function Itemlist() {
-  const [item, setItem] = useState([]);
-  const [input, setInput] = useState("");
-
-  function addItem() {
-    if (input.trim() === "") return; // prevent empty items
-      setItem([...item, input]); // 
-      setInput(""); //
-  }
-  return (
-    <div>
-      <h3>Shopping List</h3>
-      <input value={input} onChange={(e) => setInput(e.target.value)}
-        placeholder="add an item " />
-      <button onClick={addItem}>add item </button>
-      <ul>
-        {item.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
     </div>
   )
 }
