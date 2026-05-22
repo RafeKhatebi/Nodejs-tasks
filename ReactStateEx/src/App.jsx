2db250aba3bdd450d7c5fc16ddd3ed57b192b536
Rafe Khatebi <rafekhatebi@gmail.com>
2026-05-22 11:11:47 +0430

    state worked

--- ReactStateEx/src/App.jsx
+++ ReactStateEx/src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react"
-
 export default function App() {
+  const [isGoingOut, setIsGoingOut] = useState(false);
+  function changeMind() {
+    setIsGoingOut(prev => !prev)
+  }
   return (
-    <div>
-    </div>
+    <div className="container">
+      <label htmlFor="">Current answer is ${isGoingOut ? "Yes" : "No"}. Click to change it. </label>
+      <button className="value"
+        onClick={changeMind}>{isGoingOut ? "yes" : "No"}
+      </button>
+    </div >
   )
 }
